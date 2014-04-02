b3b53e29d88fb93ca6ed59a760eb6f9a79ead114
Tahira <tahira.adaya@gmail.com>
2014-04-01 21:38:50 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watchio::Application.config.secret_key_base = '9d1de423bf54143b8af32e8a0e4ed47d22a3878161045bce7bc8feb8dcbc1f3d3f9d0c40e2190283e88c273d16b827de869347d13fd87b5892891ac5b0e6cbc2'
